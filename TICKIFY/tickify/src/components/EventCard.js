7e67afae353cbb5bb227d885b9273c39cb26ff64
ronniegikandi <kiiruronnie42@gmil.com>
2025-10-24 00:36:13 +0300

    eventcard exposure

--- TICKIFY/tickify/src/components/EventCard.js
+++ TICKIFY/tickify/src/components/EventCard.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function EventCard ({event}) {
-    return (
+    return ( 
+        <div className = "event-card">
+            <img src ={event.image} alt={event.title} className="event-image"/>
+            <h2>{event.title}</h2>
+            <p>{event.date} | {event.location}</p>
+            <p>Category : {event.category}</p>
+            <p>Price : ${event.price}</p>
+            
+
+
+
+
+
+
+
+
+        </div>
+
 
 
 
