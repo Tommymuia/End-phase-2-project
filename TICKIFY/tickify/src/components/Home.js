7f859f393f7e1968ea7bd01fbf8d49f885d4c4c5
try-ronnie <kiiruronnie42@gmail.com>
2025-10-23 09:29:35 +0000

    adding conditional rendering to the list of movies

---
+++ TICKIFY/tickify/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage () {
+    return (<div className="HomePg">
+            <h1>welcone to Tickify!!!👾</h1>
+                    <p>welcome to Kenya's number one event booking sites</p>
+                            <img src=""/>
+                                    <p>we offer all events from your number 1 artists to the upcoming stars </p>
+                                            </div>
+
+                                                )
+                                                }
