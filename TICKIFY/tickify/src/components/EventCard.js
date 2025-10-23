6bfbec9e0da4b18cfbfe797370e946d00e10c400
ronniegikandi <kiiruronnie42@gmil.com>
2025-10-24 01:52:31 +0300

    redoing logic

--- TICKIFY/tickify/src/components/EventCard.js
+++ TICKIFY/tickify/src/components/EventCard.js
@@ -2,13 +2,21 @@ import React from "react";
 
 function EventCard ({event}) {
     return ( 
+        //
         <div className = "event-card">
             <img src ={event.image} alt={event.title} className="event-image"/>
             <h2>{event.title}</h2>
             <p>{event.date} | {event.location}</p>
             <p>Category : {event.category}</p>
             <p>Price : ${event.price}</p>
-            
+            <p>{event.description}</p>
+            <button>Buy Tick</button>
+        </div>
+    );
+}
+
+export default EventCard;
+
 
 
 
--- TICKIFY/tickify/src/components/NavBar.js
+++ TICKIFY/tickify/src/components/NavBar.js
@@ -1,24 +1,19 @@
-import React,{useState} from "react"
-import EventList from "./EventList"
-function NavBar(){
-    const [view , setView] = useState("home")
-// we used states to be able to change dynamically change the values of the states
-// so we run handle biew and ensure that each vaue is false so that it only shows when  a different value of the fucntion is returned upon click of the specific button and then rendered
-//theres still the option
-// have a function that renders the state to change according to the button pressed
-    function HandleView(selectView){
-        setView (selectView)
-    }
+import React from "react";
 
-    return(
-    <div id="navbar">
-        <button onClick={() => HandleView("home")}>HomeBar</button>
-        <button onClick={() => HandleView("events")}>EventBar</button>
-        <button onClick={() => HandleView("cart")}>Go To Cart</button>
-        
-    {view === "events" && <EventList/>}
-    </div>
+function NavBar ({handleView}){
+    return ( 
+        <div id="NavBar">
+            <button onClick={()=>handleView("home")}>HOME</button>
+            <button onClick={()=>handleView("events")}>EVENTS</button>
+            <button onClick={()=>handleView}
+
+        </div>
+
+
+
     )
 
+
+
+
 }
-export default NavBar
