95c4ded6fd348b8be0711911cb2d2f4945031bd4
ronniegikandi <kiiruronnie42@gmil.com>
2025-10-23 05:33:44 +0300

    changed rendering condition due to state override

--- TICKIFY/tickify/src/components/NavBar.js
+++ TICKIFY/tickify/src/components/NavBar.js
@@ -1,23 +1,25 @@
 import React,{useState} from "react"
 
 function NavBar(){
-    const [showEvents , setShowEvents] = useState(false)
-    const [showHome , setShowHome] = useState(false)
-    const [showCart , setShowCart] = useState(false)
-
-    function HandleView(view){
-        setShowHome(false)
-        setShowEvents(false)
-        setShowCart(false)
-        if(view === "home") setShowHome(true)
-        if(view === "events") setShowEvents(true)
-        
+    const [view , setView] = useState("home")
+// we used states to be able to change dynamically change the values of the states
+// so we run handle biew and ensure that each vaue is false so that it only shows when  a different value of the fucntion is returned upon click of the specific button and then rendered
+//theres still the option
+// have a function that renders the state to change according to the button pressed
+    function HandleView(selectView){
+        setView (selectView)
     }
 
     return(
     <div id="navbar">
-        <button onClick={setShowEvents(!showEvents)}>SEARCH EVENTS</button>
-        <
+        <button onClick={() => HandleView("home")}>HomeBar</button>
+        <button onClick={() => HandleView("events")}>EventBar</button>
+        <button onClick={() => HandleView("cart")}>Go To Cart</button>
+        
+    {view === "home" && <ShowHome /> }
+    {view === "events" && <ShowEvents />}
+    {view === "cart" && <ShowCart />}
     </div>
     )
+
 }
