74d00050914feb85dc4f2b0d5e984c04fb49d6f4
ronniegikandi <kiiruronnie42@gmil.com>
2025-10-24 10:57:59 +0300

    working on the sign up component

--- TICKIFY/tickify/src/components/EventList.js
+++ TICKIFY/tickify/src/components/EventList.js
@@ -14,20 +14,24 @@ function EventList (){
   // useeffeect to bring in the json since it smth run out of the code render (async)
   //we then save that data in state to cause another rerender to now display the ui if the condtion is met
   //we use defensive rendering incase of json errors ... this is just condtional rendering only if the state event is change d according to the values passed
-  return (
-    
-    <div className="event-list">
-      {events.length === 0 ? (
-        <p>loading events .... please wait</p>
-      ) : (
-        events.map((event) => (
-          <div key={event.id} className="eventcard">
-        )
-          
-        
-      )}
-
-
-    </div>
-  )
+return (
+  <div className="event-list">
+    {events.length === 0 ? (
+      <p>Loading events... please wait</p>
+    ) : (
+      events.map((event) => (
+        <div key={event.id} className="eventcard">
+          <img src={event.image} alt={event.title} />
+          <h2>{event.title}</h2>
+          <p>{event.date}</p>
+          <p>{event.location}</p>
+          <p>Ksh {event.price}</p>
+          <button>Buy Ticket</button>
+        </div>
+      ))
+    )}
+  </div>
+)
 }
+export default EventList ;
+
