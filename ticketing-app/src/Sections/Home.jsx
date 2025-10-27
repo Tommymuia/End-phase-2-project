fd0c9b14387712425e75d6147420746a4f890f62
Tommymuia <muiathomas.mt@gmail.com>
2025-10-27 22:12:40 +0300

    added

--- ticketing-app/src/Sections/Home.jsx
+++ ticketing-app/src/Sections/Home.jsx
@@ -13,13 +13,52 @@ function Home() {
       document.body.classList.remove("no-scroll");
     };
   }, []);
+  const eventImages = [
+    "/1.jpg",
+    "/2.jpg",
+    "/3.jpg",
+    "/4.jpg",
+    "/5.jpg",
+    "/6.jpg",
+    "/7.jpg",
+    "/8.jpg",
+  ];
 
   return (
     <section id="home" className="home-section">
+      <div className="carousel-container">
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          spaceBetween={30}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          loop={true}
+          className="home-swiper"
+        >
+          {eventImages.map((event, index) => (
+            <SwiperSlide key={index}>
+              <div className="slide-content">
+                <img
+                  src={event}
+                  alt={`event${index + 1}`}
+                  className="carousel-image"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
       <div className="home-content">
         <h1>Welcome to Tickify</h1>
-        <p>Book, manage and explore your favorite events with ease and confidence.</p>
-        <a href="/events" className="explore-btn">Explore Events</a>
+        <p>
+          Book, manage and explore your favorite events with ease and
+          confidence.
+        </p>
+        <a href="/events" className="explore-btn">
+          Explore Events
+        </a>
       </div>
     </section>
   );
