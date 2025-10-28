740edb3304cb309cbceab895c80a45cf10cc874c
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-10-28 12:38:21 +0300

    added to sections

--- ticketing-app/src/Sections/Contacts.jsx
+++ ticketing-app/src/Sections/Contacts.jsx
@@ -3,4 +3,7 @@ import React from "react";
  function Contacts() {
   return (
     <div>
-     
+      <h1>Contact Us</h1>
+      <p>Need help with a ticket, refund or event information? We’re here to assist you.</p>
+
+      
