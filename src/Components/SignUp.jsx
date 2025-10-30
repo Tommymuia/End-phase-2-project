c3a96b2aa19e5faf9b22fd28d15a5f4072c20e1c
Tommymuia <muiathomas.mt@gmail.com>
2025-10-30 09:54:12 +0300

    added

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -20,7 +20,7 @@ function SignUp() {
     const newUser = { firstName, lastName, email, password };
 
     try {
-      const response = await fetch("https://json-server-vercel-five-flax.vercel.app/users", {
+      const response = await fetch("http://localhost:3001/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newUser),
